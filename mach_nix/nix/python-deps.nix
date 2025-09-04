74f03ffbdf24ec2de21e801c420362a5a1c28173
GlassGhost <GlassGhost@users.noreply.github.com>
2025-09-03 22:26:43 -0500

    Update python-deps.nix
    use git for new version of distlib resolvelib and tree-format

--- mach_nix/nix/python-deps.nix
+++ mach_nix/nix/python-deps.nix
@@ -1,11 +1,11 @@
-{python, fetchurl, ...}:
+{python, fetchGit, fetchurl, ...}:
 rec {
   distlib = python.pkgs.buildPythonPackage {
-    name = "distlib-0.3.7.dev.0";
-    src = fetchurl {
-      url = "https://github.com/pypa/distlib/archive/05375908c1b2d6b0e74bdeb574569d3609db9f56.zip";
-      sha256 = "qbaJlmAkp5V8l1bWzJFar+6GiRiUyNMJAW6Yi1xZ088=";
-    };
+    name = "distlib-0.4.0";
+    src = fetchGit {
+    url = "https://github.com/pypa/distlib";
+    rev = "aff2cbbc4e8a87b9440cb93472789a213813dbd6"; # 0.4.0
+  };
     format = "pyproject";
     doCheck = false;
     propagatedBuildInputs = [
@@ -13,19 +13,19 @@ rec {
     ];
   };
   resolvelib = python.pkgs.buildPythonPackage {
-    name = "resolvelib-0.8.1";
-    src = fetchurl {
-      url = "https://files.pythonhosted.org/packages/ac/20/9541749d77aebf66dd92e2b803f38a50e3a5c76e7876f45eb2b37e758d82/resolvelib-0.8.1.tar.gz";
-      sha256 = "c6ea56732e9fb6fca1b2acc2ccc68a0b6b8c566d8f3e78e0443310ede61dbd37";
-    };
+    name = "resolvelib-1.2.0";
+    src = fetchGit {
+    url = "https://github.com/sarugaku/resolvelib";
+    rev = "50f221ba803f6c127b6e20ff2c9a4470524e1239"; # 1.2.0
+  };
     doCheck = false;
   };
   tree-format = python.pkgs.buildPythonPackage {
     name = "tree-format-0.1.2";
-    src = fetchurl {
-      url = "https://files.pythonhosted.org/packages/0d/91/8d860c75c3e70e6bbec7b898b5f753bf5da404be9296e245034360759645/tree-format-0.1.2.tar.gz";
-      sha256 = "a538523aa78ae7a4b10003b04f3e1b37708e0e089d99c9d3b9e1c71384c9a7f9";
-    };
+    src = fetchGit {
+    url = "https://github.com/jml/tree-format";
+    rev = "922c9d37661dfb884cd45a9a9a2533f5eabe09f5"; # 6yrs unchanged
+  };
     doCheck = false;
   };
 
