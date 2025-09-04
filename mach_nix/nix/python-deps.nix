dd5d0f82a3b24034b937c0fb4efc67593577dab3
GlassGhost <GlassGhost@users.noreply.github.com>
2025-09-03 22:52:07 -0500

    Update python-deps.nix

--- mach_nix/nix/python-deps.nix
+++ mach_nix/nix/python-deps.nix
@@ -5,6 +5,7 @@ rec {
     src = fetchgit {
     url = "https://github.com/pypa/distlib";
     rev = "aff2cbbc4e8a87b9440cb93472789a213813dbd6"; # 0.4.0
+    sha256 = "1vvhclqf1bczq7av1xsyzqi1cgbdaig42fld052z90khnbw29381";
   };
     format = "pyproject";
     doCheck = false;
@@ -17,6 +18,7 @@ rec {
     src = fetchgit {
     url = "https://github.com/sarugaku/resolvelib";
     rev = "50f221ba803f6c127b6e20ff2c9a4470524e1239"; # 1.2.0
+    sha256 = "0467czv9fbj78yi1ql5ia1xpcap6gmq16pwacg51rzjvk7ackxzi";
   };
     doCheck = false;
   };
@@ -25,6 +27,7 @@ rec {
     src = fetchgit {
     url = "https://github.com/jml/tree-format";
     rev = "922c9d37661dfb884cd45a9a9a2533f5eabe09f5"; # 6yrs unchanged
+    sha256 = "1j4y9jlqz434k59l07nnkg6b5mx7cywll0lkd3h1gg0xxapj14fm";
   };
     doCheck = false;
   };
